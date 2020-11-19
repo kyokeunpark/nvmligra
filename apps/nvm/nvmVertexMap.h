986e7327b8257b7d5e665280ee995eb7c79db946
siyue <siyuew1@cs.toronto.edu>
2020-11-19 12:18:20 -0800

    a working version nvmPageRank

---
+++ apps/nvm/nvmVertexMap.h
@@ -0,0 +1,87 @@
+
+template <class F, class VS, typename std::enable_if<
+  !std::is_same<VS, nvmVertexSubset>::value, int>::type=0 >
+void nvmVertexMap(VS& V, F f) {
+  size_t n = V.numRows(), m = V.numNonzeros();
+  if(V.dense()) {
+    parallel_for(long i=0;i<n;i++) {
+      if(V.isIn(i)) {
+        f(i, V.ithData(i));
+      }
+    }
+  } else {
+    parallel_for(long i=0;i<m;i++) {
+      f(V.vtx(i), V.vtxData(i));
+    }
+  }
+}
+
+template <class VS, class F, typename std::enable_if<
+  std::is_same<VS, nvmVertexSubset>::value, int>::type=0 >
+void nvmVertexMap(VS& V, F f) {
+  size_t n = V.numRows(), m = V.numNonzeros();
+  if(V.dense()) {
+    parallel_for(long i=0;i<n;i++) {
+      if(V.isIn(i)) {
+        f(i);
+      }
+    }
+  } else {
+    parallel_for(long i=0;i<m;i++) {
+      f(V.vtx(i));
+    }
+  }
+}
+
+//Note: this is the version of vertexMap in which only a subset of the
+//input nvmVertexSubset is returned
+template <class F>
+nvmVertexSubset vertexFilter(nvmVertexSubset V, F filter) {
+  long n = V.numRows(), m = V.numNonzeros();
+  V.toDense();
+  bool* d_out = newA(bool,n);
+  {parallel_for(long i=0;i<n;i++) d_out[i] = 0;}
+  {parallel_for(long i=0;i<n;i++)
+      if(V.d[i]) d_out[i] = filter(i);}
+  return nvmVertexSubset(n,d_out);
+}
+
+template <class F>
+nvmVertexSubset vertexFilter2(nvmVertexSubset V, F filter) {
+  long n = V.numRows(), m = V.numNonzeros();
+  if (m == 0) {
+    return nvmVertexSubset(n);
+  }
+  bool* bits = newA(bool, m);
+  V.toSparse();
+  {parallel_for(size_t i=0; i<m; i++) {
+    uintE v = V.vtx(i);
+    bits[i] = filter(v);
+  }}
+  auto v_imap = make_in_imap<uintE>(m, [&] (size_t i) { return V.vtx(i); });
+  auto bits_m = make_in_imap<bool>(m, [&] (size_t i) { return bits[i]; });
+  auto out = pbbs::pack(v_imap, bits_m);
+  out.alloc = false;
+  free(bits);
+  return nvmVertexSubset(n, out.size(), out.s);
+}
+
+template <class data, class F>
+nvmVertexSubset vertexFilter2(nvmVertexSubsetData<data> V, F filter) {
+  long n = V.numRows(), m = V.numNonzeros();
+  if (m == 0) {
+    return nvmVertexSubset(n);
+  }
+  bool* bits = newA(bool, m);
+  V.toSparse();
+  parallel_for(size_t i=0; i<m; i++) {
+    auto t = V.vtxAndData(i);
+    bits[i] = filter(std::get<0>(t), std::get<1>(t));
+  }
+  auto v_imap = make_in_imap<uintE>(m, [&] (size_t i) { return V.vtx(i); });
+  auto bits_m = make_in_imap<bool>(m, [&] (size_t i) { return bits[i]; });
+  auto out = pbbs::pack(v_imap, bits_m);
+  out.alloc = false;
+  free(bits);
+  return nvmVertexSubset(n, out.size(), out.s);
+}
