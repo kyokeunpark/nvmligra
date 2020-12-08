57043332e5b04f04517ee79a79cd34a3f24a7811
KyoKeun Park <kyokeunpark@gmail.com>
2020-12-08 10:06:07 -0500

    Remove unnecessary timing code

--- ligra/gettime.h
+++ ligra/gettime.h
@@ -70,7 +70,6 @@ struct timer {
 
   double next() {
     if (!on) return 0.0;
-    std::cout << "testing" << std::endl;
     double t = getTime();
     double td = t - lastTime;
     totalTime += td;
@@ -79,7 +78,7 @@ struct timer {
   }
 
   void reportT(double time) {
-    std::cout << std::setprecision(5) << time <<  std::endl;;
+    std::cout << std::setprecision(3) << time <<  std::endl;;
   }
 
   void reportTime(double time) {
